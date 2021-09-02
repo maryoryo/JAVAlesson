c81c256ece5f10d781a9773fcb52d66c4e4441e5
maryoryo名 <liyomaxxde@gmail.com>
2021-09-03 01:05:16 +0900

    JAVAstart

---
+++ Myapp.java
@@ -0,0 +1,54 @@
+public class Myapp {
+  // comment
+  /*
+  */
+  
+  public static void main(String[] args) {
+    
+    // char a = 'a';
+
+    // int x = 10;
+
+    // long y = 55555555555L;
+
+    // double d = 23243.542;
+
+    // float f = 32.33F;
+
+    // boolean flag = true;
+
+    // String msg;
+    // msg = "Hello World Again!";
+
+    // String msg = "Hello World Again!!!";
+    // System.out.println(msg);
+
+    // int i;
+
+    // i = 10 / 3;
+    // System.out.println(i);
+    // i = 10 % 3;
+    // System.out.println(i);
+    // int x = 5;
+    // x++;
+    // System.out.println(x);
+    // x--;
+    // System.out.println(x);
+
+    // int x = 5;
+    // x = x + 12;
+    // System.out.println(x);
+
+    // String s;
+    // s= "hello" + "world";
+    // System.out.println(s);
+
+    // double d = 54333.23;
+    // int i = (int)d;
+    // System.out.println(i);
+
+    int i = 10;
+    double d = (double)i / 4;
+    System.out.println(d);
+  }
+}
