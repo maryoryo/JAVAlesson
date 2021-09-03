31e3adec733c52aedc323f61553f7c1595800fd5
maryoryo名 <liyomaxxde@gmail.com>
2021-09-03 21:03:41 +0900

    java3

--- Myapp.java
+++ Myapp.java
@@ -193,54 +193,527 @@
     
     // }
 
-  class User {
-    String name;
+  // class User {
+  //   String name;
 
-    User(String name) {
-      this.name = name;
-    }
+  //   User(String name) {
+  //     this.name = name;
+  //   }
     
-    User() {
-      // this.name = "Me!";
-      this("Me!");
-    }
+  //   User() {
+  //     // this.name = "Me!";
+  //     this("Me!");
+  //   }
 
-    void sayHi() {
-      System.out.println("Hi!" + this.name);
-    }
-  }
+  //   void sayHi() {
+  //     System.out.println("Hi!" + this.name);
+  //   }
+  // }
 
 
-  class AdminUser extends User {
+  // class AdminUser extends User {
 
-    AdminUser(String name) {
-      super(name);
-    }
+  //   AdminUser(String name) {
+  //     super(name);
+  //   }
 
-    void sayHello() {
-      System.out.println("Hello!" + this.name);
-    }
+  //   void sayHello() {
+  //     System.out.println("Hello!" + this.name);
+  //   }
 
-    // @Override
-    void sayHi() {
-      System.out.println("[admin] Hi!" + this.name);
-    }
+  //   // @Override
+  //   void sayHi() {
+  //     System.out.println("[admin] Hi!" + this.name);
+  //   }
 
-  }
+  // }
 
     
-  public class Myapp {
-    public static void main(String[] args) {
+  // public class Myapp {
+  //   public static void main(String[] args) {
 
-      User tom = new User("Tom");
-      System.out.println(tom.name);
-      tom.sayHi();
+  //     User tom = new User("Tom");
+  //     System.out.println(tom.name);
+  //     tom.sayHi();
 
-      AdminUser bob = new AdminUser("bob");
-      System.out.println(bob.name);
-      bob.sayHi();
-      bob.sayHello();
-    }
+  //     AdminUser bob = new AdminUser("bob");
+  //     System.out.println(bob.name);
+  //     bob.sayHi();
+  //     bob.sayHello();
+  //   }
+  // }
+
+
+// class User {
+//   private String name;
+//   private int score;
+
+//   public User(String name, int score) {
+//     this.name = name;
+//     this.score = score;
+//   }
+
+//   public int getScore() { //getter
+//     return this.score;
+//   }
+
+//   public void setScore(int score) { //setter
+//     if (score > 0) {
+//       this.score = score;
+//     }
+//   }
+// }
+
+// public class Myapp {
+
+//   private static int score;
+
+//   public static void main(String[] args) {
+//     User tom = new User("tom", 65);
+//     tom.setScore(85);
+//     tom.setScore(-22);
+//     score = tom.getScore();
+//     System.out.println(score);
+//   }
+// }
+
+
+// class User {
+//   private String name;
+//   private static int count;
+
+//   static {
+//     User.count = 0;
+//     System.out.println("Static initializer");
+//   }
+
+//   {
+//     System.out.println("instance initializer");
+//   }
+
+//   public User(String name) {
+//     this.name = name;
+//     User.count++;
+//     System.out.println("Constructor");
+//   }
+
+//   public static void getInfo() {
+//     System.out.println("# of instances: " + User.count);
+//   }
+// }
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+//     User.getInfo();
+//     User tom = new User("tom");
+//     User.getInfo();
+//     User bob = new User("bob");
+//     User.getInfo();
+//   }
+// }
+
+
+// abstract class User {
+//   public abstract void sayHi();
+// }
+
+// class JapaneseUser extends User {
+//   @Override
+//   public void sayHi() {
+//     System.out.println("こんにちは");
+//   }
+// }
+
+// class AmericanUser extends User {
+//   @Override
+//   public void sayHi() {
+//     System.out.println("Hi");
+//   }
+// }
+
+// public class Myapp {
+//   public static void main(String[] args) {
+//     AmericanUser tom = new AmericanUser();
+//     JapaneseUser aki = new JapaneseUser();
+//     // System.out.println("hi!");
+//     tom.sayHi();
+//     aki.sayHi();
+//   }
+// }
+
+
+// interface Printable {
+//   //定数
+//   double VERSION = 1.2;
+  
+//   void print();
+//   //default
+//   public default void getInfo() {
+//     System.out.println("I/F ver." + Printable.VERSION);
+//   }
+//   //static
+
+// }
+
+// class User implements Printable {
+//   @Override
+//   public void print() {
+//     System.out.println("Now printing user profile ...");
+//   }
+// }
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+//     User tom = new User();
+//     tom.print();
+//     tom.getInfo();
+//   }
+
+// }
+
+
+// enum Result {
+//   SUCCESS,
+//   ERROR,
+// }
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+//     Result res;
+
+//     res = Result.ERROR;
+
+//     switch (res) {
+//       case SUCCESS:
+//         System.out.println("OK");
+//         System.out.println(res.ordinal());
+//         break;
+//       case ERROR:
+//         System.out.println("NG");
+//         System.out.println(res.ordinal());
+//         break;
+//     }
+    
+//   }
+// }
+
+
+// class MyException extends Exception {
+//   public MyException(String s) {
+//     super(s);
+//   }
+// }
+
+// public class Myapp {
+
+//   public static void div(int a, int b) {
+//     try {
+//       if (b < 0) {
+//         throw new MyException("not nimus");
+//       }
+//       System.out.println(a / b);
+//     } catch (ArithmeticException e) {
+//       System.err.println(e.getMessage());
+//     } catch (MyException e) {
+//       System.err.println(e.getMessage());
+//     } finally {
+//       System.out.println("--end--");
+//     }
+//   }
+
+//   public static void main(String[] args) {
+//     div(3, 0);
+//     div(5, -2);
+//   }
+// }
+
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+//     // Integer i = new Integer(32);
+//     // int n = i.intValue();
+
+//     Integer i = 32;
+//     i = null;
+//     int n = i;
+//   }
+
+// }
+
+
+
+// class MyInteger {
+//   public void getThree(int x) {
+//     System.out.println(x);
+//     System.out.println(x);
+//     System.out.println(x);
+//   }
+// }
+
+// class MyDate<T> {
+//   public void getThree(T x) {
+//     System.out.println(x);
+//     System.out.println(x);
+//     System.out.println(x);
+//   }
+// }
+
+// public class Myapp {
+  
+//   public static void main(String[] args) {
+//     // MyInteger mi = new MyInteger();
+//     // mi.getThree(55);
+//     MyDate<Integer> i = new MyDate<>();
+//     i.getThree(32);
+//     MyDate<String> s = new MyDate<>();
+//     s.getThree("Hello");
+//   }
+// }
+
+
+// class MyRunnable implements Runnable {
+//   @Override
+//   public void run() {
+//     for(int i = 0; i < 500; i++) {
+//       System.out.print('*');
+//     }
+//   }
+// }
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+//     // MyRunnable r = new MyRunnable();
+//     // Thread t = new Thread(r);
+//     // t.start();
+
+//     // new Thread(new Runnable() {
+//     //   @Override
+//     //   public void run() {
+//     //     for(int i = 0; i < 500; i++) {
+//     //       System.out.print('*');
+//     //     }
+//     //   }
+//     // }).start();
+    
+//     new Thread(() -> {
+      
+//       for(int i = 0; i < 500; i++) {
+//         System.out.print('*');
+//       }
+    
+//     }).start();
+
+
+
+//     for(int i = 0; i < 500; i++) {
+//       System.out.print('.');
+//     }
+//   }
+
+// }
+
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+//     String s = "abcdef";
+//     System.out.println(s.length());
+//     System.out.println(s.substring(2, 5));
+//     System.out.println(s.replaceAll("cd", "CD"));
+
+//     String s1 = "ab";
+//     String s2 = "ab";
+
+//     if (s1.equals(s2)) {
+//       System.out.println("same");
+//     }
+    
+//     if (s1 == s2) {
+//       System.out.println("same same");
+//     }
+    
+//     String ss1 = new String("ab");
+//     String ss2 = new String("ab");
+    
+//     if (ss1 == ss2) {
+//       System.out.println("same same same");
+//     }
+//   }
+
+// }
+
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+    
+//     int score = 50;
+//     double height = 165.8;
+//     String name = "taguchi";
+
+//     System.out.printf("name: %s, score: %d, height: %f\n", name, score, height);
+//     System.out.printf("name: %-10s, score: %10d, height: %5.2f\n", name, score, height);
+
+//     String s = String.format("name: %10s, score: %-10d, height: %5.2f\n", name, score, height);
+//     System.out.println(s);
+//   }
+
+// }
+
+
+// import java.util.Random;
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+//     double d = 53.234;
+//     System.out.println(Math.ceil(d));
+//     System.out.println(Math.floor(d));
+//     System.out.println(Math.round(d));
+//     System.out.println(Math.PI);
+
+//     Random r = new Random();
+//     System.out.println(r.nextDouble());
+//     System.out.println(r.nextInt(100));
+//     System.out.println(r.nextBoolean());
+//   }
+// }
+
+
+// import java.util.*;
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+
+//     // ArrayList<Integer> sales = new ArrayList<>();
+//     List<Integer> sales = new ArrayList<>();
+
+//     sales.add(10);
+//     sales.add(20);
+//     sales.add(30);
+
+//     for (int i = 0; i < sales.size(); i++) {
+//       System.out.println(sales.get(i));
+//     }
+
+//     sales.set(0, 100);
+//     sales.remove(2);
+    
+//     for (Integer sale : sales) {
+//       System.out.println(sale);
+//     }
+
+//   }
+// }
+
+
+// import java.util.*;
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+
+//     // HashSet<Integer> sales = new HashSet<>();
+//     Set<Integer> sales = new LinkedHashSet<>();
+
+//     sales.add(10);
+//     sales.add(20);
+//     sales.add(30);
+//     sales.add(10);
+    
+//     System.out.println(sales.size());
+
+//     for (Integer sale : sales) {
+//       System.out.println(sale);
+//     }
+    
+//     sales.remove(30);
+    
+//     for (Integer sale : sales) {
+//       System.out.println(sale);
+//     }
+//   }
+// }
+
+
+// import java.util.*;
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+
+//     // HashMap<String, Integer> sales = new HashMap<>();
+//     Map<String, Integer> sales = new HashMap<>();
+
+//     sales.put("tom", 10);
+//     sales.put("bob", 20);
+//     sales.put("steve", 30);
+
+//     System.out.println(sales.get("tom"));
+//     System.out.println(sales.size());
+
+//     for (Map.Entry<String, Integer> sale : sales.entrySet()) {
+//       System.out.println(sale.getKey() + ":" + sale.getValue());
+//     }
+
+//     sales.put("tom", 100);
+//     sales.remove("steve");
+
+//     for (Map.Entry<String, Integer> sale : sales.entrySet()) {
+//       System.out.println(sale.getKey() + ":" + sale.getValue());
+//     }
+
+//   }
+// }
+
+
+// import java.util.*;
+
+// public class Myapp {
+
+//   public static void main(String[] args) {
+    
+//     List<Integer> sales = new ArrayList<>(Arrays.asList(12, 30, 22, 4, 9));
+//     // for (Integer sale : sales) {
+//     //   System.out.println(sale);
+//     // }
+
+//     sales
+//       .stream()
+//       .filter(e -> e % 3 == 0)
+//       .map(e -> "(" + e + ")")
+//       .forEach(System.out::println);
+
+//   }
+// }
+
+
+
+import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Myapp {
+
+  public static void main(String[] args) {
+    LocalDateTime d = LocalDateTime.now();
+    // LocalDateTime d = LocalDateTime.of(2016, 1, 1, 10, 10, 10);
+    // LocalDateTime d = LocalDateTime.parse("2016-01-01T10:10:10");
+    
+    System.out.println(d.getYear());
+    System.out.println(d.getMonth());
+    System.out.println(d.getMonth().getValue());
+    
+    System.out.println(d.plusMonths(2).minusDays(3));
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy!MM!dd!");
+    System.out.println(d.format(dtf));
   }
-
+} 
 
---
+++ com/dotinstall/myapp/model/User.java
@@ -0,0 +1,15 @@
+package com.dotinstall.myapp.model;
+
+public class User {
+  
+  protected String name;
+
+  public User(String name) {
+    this.name = name;
+  }
+
+  public void sayHi() {
+    System.out.println("hi! " + this.name);
+  }
+
+}
